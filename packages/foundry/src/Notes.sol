e6f0ddbc8bc0fd3679336ff050e61ac7878534ca
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-07-12 22:24:13 +0200

    updated token

--- packages/foundry/src/Notes.sol
+++ packages/foundry/src/Notes.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
+/* ChainNotes Libraries */
 import {CNDataTypes} from "src/libraries/CNDataTypes.sol";
 
+/**
+ * @title Notes
+ * @author ChainNotes Technical Team
+ * @notice Main contract in charge of vot
+ *
+ */
 contract Notes {
     uint16 public constant DENOMINATOR = 10_000;
 
@@ -12,25 +19,45 @@ contract Notes {
 
     mapping(address contractAddress => CNDataTypes.Note[] note) public notesOf;
 
-    mapping(address user => CNDataTypes.User info) public infoOfUser;
+    mapping(address contractAddress => mapping(CNDataTypes.Sentiment sentiment => uint16 amount)) public sentimentOf;
+
+    mapping(address contractAddress => mapping(uint16 index => mapping(CNDataTypes.Rating rating => uint32 amount))) public amountOfRating;
+
+    mapping(address user => mapping(CNDataTypes.Rating => uint40 amount)) public ratingWeightOf;
 
     mapping(address user => mapping(address contractAddress => mapping(uint16 index => CNDataTypes.Rating))) public userRatingOfNote;
 
-    function newNote(
+    function publishNote(
         address _contractAddress,
         string calldata _uri,
-        string calldata _pictureUri,
-        CNDataTypes.Sentiment _sentiment,
-    ) external returns (CNDataTypes.Note _note) {}
+        CNDataTypes.Sentiment _sentiment
+    ) external returns(CNDataTypes.Note memory _note) {
+        _note = CNDataTypes.Note({
+            noteWriter: msg.sender,
+            uri: _uri,
+            score: 0,
+            sentiment: _sentiment
+        });
+
+        notesOf[_contractAddress].push(_note);
+
+        sentimentOf[_contractAddress][_sentiment]++;
+    }
 
     function vote(
         CNDataTypes.Rating _rating,
         uint16 _noteIndex,
         address _contractAddress
-    ) external {}
+    ) external {
+        ratingWeightOf[msg.sender][_rating]++;
 
-    fucntion tip(
-        uint16 _noteIndex,
-        address _contractAddress
-    ) payable external returns (bool _ok) {}
+        userRatingOfNote[msg.sender][_contractAddress][_noteIndex] = _rating;
+
+        amountOfRating[_contractAddress][_noteIndex][_rating]++;
+    }
+
+    // fucntion tip(
+        // uint16 _noteIndex,
+        // address _contractAddress
+    // ) payable external returns (bool _ok) {}
 }
--- packages/foundry/src/libraries/CNDataTypes.sol
+++ packages/foundry/src/libraries/CNDataTypes.sol
@@ -4,16 +4,9 @@ pragma solidity 0.8.26;
 library CNDataTypes {
     struct Note {
         address noteWriter;
-        mapping(Rating rating => uint32 amount) amountOfRating;
-        mapping(Sentiment sentiment => uint32 amount) amountOfSentiment;
+        Sentiment sentiment;
         uint16 score;
         string uri;
-        string pictureUri;
-    }
-
-    struct User {
-        uint40 amountOfVotes;
-        mapping(Rating rating => uint40) amountOf;
     }
 
     enum Rating {
