285008832f9294d0708f5cb34750804ec42f7ff4
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-07-13 16:56:42 +0200

    updated deployment scripts

--- packages/foundry/src/libraries/CNDataTypes.sol
+++ packages/foundry/src/libraries/CNDataTypes.sol
@@ -1,19 +1,51 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.26;
 
+/**
+ * @title CNDDataTypes
+ * @author ChainNotes Technical Team
+ * @notice Library containing ChainNotes contracts' custom DataTypes
+ *
+ */
 library CNDataTypes {
+    /**
+     * @notice
+     *  Custom note datatype
+     *
+     * @param noteWriter address of author
+     * @param sentiment if comment is POSITIVE or NEGATIVE
+     * @param uri IPFS uri as a string
+     *
+     */
     struct Note {
         address noteWriter;
         Sentiment sentiment;
-        uint16 score;
         string uri;
     }
 
+    /**
+     * @notice
+     *  Custom datatype to keep track of the note score
+     *
+     * @param score score of specific note
+     * @param consideredHelpful a bool if this note is considered helpful
+     *
+     */
     struct NoteScore {
         uint256 score;
         bool consideredHelpful;
     }
 
+    /**
+     * @notice
+     *  Custom datatype to handle world id proof
+     *
+     * @param root The root of the Merkle tree (returned by the JS widget).
+     * @param signal signal An arbitrary input from the user, usually the user's wallet address
+     * @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget)
+     * @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
+     *
+     */
     struct WorldIdProof {
         uint256 root;
         address signal;
@@ -21,12 +53,14 @@ library CNDataTypes {
         uint256[8] proof;
     }
 
+    /// @dev Rating enum, used to rate different notes
     enum Rating {
         HELPFUL,
         NOT_HELPFUL,
         SOMEWHAT_HELPFUL
     }
 
+    /// @dev Sentiment enum, used to tag a note as either positive or negative in sentiment
     enum Sentiment {
         POSITIVE,
         NEGATIVE
--- packages/foundry/src/libraries/CNEvents.sol
+++ packages/foundry/src/libraries/CNEvents.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.26;
 
+/* ChainNotes libraries */
 import {CNDataTypes} from "src/libraries/CNDataTypes.sol";
 
+/**
+ * @title CNDEvents
+ * @author ChainNotes Technical Team
+ * @notice Library containing ChainNotes contracts' custom events
+ *
+ */
 library CNEvents {
+    /// @dev Event emitted when a note is published
     event NotePublished(
         address indexed author,
         address indexed contractAddress,
         uint256 amountOfNotesForContract,
         CNDataTypes.Note note
-
     );
 
+    /// @dev Event emitted when a user has voted
     event Voted(
         address indexed voter,
         address indexed contractAddress,
@@ -20,6 +28,7 @@ library CNEvents {
         uint256 score
     );
 
+    /// @dev Event emitted when someone has tipped an author
     event Tipped(
         address indexed tipper,
         address indexed author,
